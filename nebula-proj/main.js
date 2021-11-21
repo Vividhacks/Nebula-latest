251b29550fdb04b554784af7d6f422e26f69c395
Arya Holmukhe <68239007+Aryaholmukhe@users.noreply.github.com>
2021-11-21 10:51:32 -0500

    3rd commit
    changes regarding the planet interaction with mouse

--- nebula-proj/main.js
+++ nebula-proj/main.js
@@ -7,24 +7,48 @@ import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
 
 const scene = new THREE.Scene();
 
+const fov = 60;
+const aspect = 2;  // the canvas default
+const near = 0.1;
+const far = 200;
+const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+const canvas= document.querySelector("#bg")
 
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
+camera.position.z = 30;
+ 
+ 
+// put the camera on a pole (parent it to an object)
+// so we can spin the pole to move the camera around the scene
+const cameraPole = new THREE.Object3D();
+scene.add(cameraPole);
+cameraPole.add(camera);
+
+const color = 0xFFFFFF;
+const intensity = 0.3;
+const light = new THREE.DirectionalLight(color, intensity);
+light.position.set(-1, 2, 4);
+scene.add(light);
+
+// const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
 
 camera.position.set(0,2.5,2.5);
 const renderer = new THREE.WebGLRenderer({
-  canvas: document.querySelector("#bg")
+  canvas
 })
 
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight)
+ 
 
-renderer.render(scene, camera)
 
-const pointLight = new THREE.PointLight("#c9c9c9s");
-pointLight.position.set(20,20,20);
+
+
+// const pointLight = new THREE.PointLight("#c9c9c9s");
+// pointLight.position.set(20,20,20);
 
 const ambientLight = new THREE.AmbientLight("#eae6ff")
-scene.add(pointLight, ambientLight);
+// scene.add(pointLight, ambientLight);
+scene.add(ambientLight)
 
 
 function addStar(){
@@ -128,7 +152,7 @@ scene.add(saturn)
 
 // saturn ring
 const ringTexture = new THREE.TextureLoader().load("./images/saturnRings.png")
-const ring = new THREE.RingGeometry(4.5, 15, 64);
+const ring = new THREE.RingGeometry(2.5, 20, 64);
 var pos = ring.attributes.position;
 var v3 = new THREE.Vector3();
 for (let i = 0; i < pos.count; i++){
@@ -164,19 +188,107 @@ const neptune = new THREE.Mesh(
 )
 scene.add(neptune)
 
-// 3d mouse
-document.addEventListener('mousedown', onMouseDown);
-function onMouseDown( event ){
-    event.preventDefault();
-// x
-    const mouse3D = new THREE.Vector3( (event.clientX / window.innerWidth) * 2 - 1, -(event.clientY / window.innerheight) * 2 - 1, 0.5 )
-    const raycaster = new THREE.Raycaster()
-    raycaster.setFromCamera(mouse3D, camera)
-    const intersects = raycaster.intersectObjects([mercury, venus, earth, mars, jupiter, saturn, uranus, neptune]);
-if(intersects.length > 0){
-      intersects[0].object.material.setSize( Math.random()*10 )
+
+
+
+
+
+
+ class PickHelper {
+  constructor() {
+    this.raycaster = new THREE.Raycaster();
+    this.pickedObject = null;
+    this.pickedObjectSavedSize = 10;
+  }
+  pick(normalizedPosition, scene, camera, time) {
+    // restore the color if there is a picked object
+    if (this.pickedObject) {
+      this.pickedObject.material.emissive.setSize(this.pickedObjectSavedSize);
+      this.pickedObject = undefined;
+    }
+ 
+    // cast a ray through the frustum
+    this.raycaster.setFromCamera(normalizedPosition, camera);
+    // get the list of objects the ray intersected
+    const intersectedObjects = this.raycaster.intersectObjects(scene.children);
+    if (intersectedObjects.length) {
+      // pick the first object. It's the closest one
+      this.pickedObject = intersectedObjects[0].object;
+      // save its color
+      this.pickedObjectSavedColor = this.pickedObject.material.emissive.getHex();
+      // set its emissive color to flashing red/yellow
+      this.pickedObject.material.emissive.setHex((time * 8) % 2 > 1 ? 0xFFFF00 : 0xFF0000);
     }
   }
+}
+
+const pickPosition = {x: 0, y: 0};
+const pickHelper = new PickHelper();
+clearPickPosition();
+ 
+ 
+function render(time) {
+  time *= 0.001;
+
+  cubes.forEach((cube, ndx) => {
+    const speed = 1 + ndx * .1;
+    const rot = time * speed;
+    cube.rotation.x = rot;
+    cube.rotation.y = rot;
+  });
+
+ 
+  pickHelper.pick(pickPosition, scene, camera, time);
+ 
+  renderer.render(scene, camera);
+
+  requestAnimationFrame(render);
+}
+
+function getCanvasRelativePosition(event) {
+  const rect = canvas.getBoundingClientRect();
+  return {
+    x: (event.clientX - rect.left) * canvas.width  / rect.width,
+    y: (event.clientY - rect.top ) * canvas.height / rect.height,
+  };
+}
+ 
+function setPickPosition(event) {
+  const pos = getCanvasRelativePosition(event);
+  pickPosition.x = (pos.x / canvas.width ) *  2 - 1;
+  pickPosition.y = (pos.y / canvas.height) * -2 + 1;  // note we flip Y
+}
+ 
+function clearPickPosition() {
+  // unlike the mouse which always has a position
+  // if the user stops touching the screen we want
+  // to stop picking. For now we just pick a value
+  // unlikely to pick something
+  pickPosition.x = -100000;
+  pickPosition.y = -100000;
+}
+ 
+window.addEventListener('mousemove', setPickPosition);
+window.addEventListener('mouseout', clearPickPosition);
+window.addEventListener('mouseleave', clearPickPosition);
+
+// mobile support
+window.addEventListener('touchstart', (event) => {
+  // prevent the window from scrolling
+  event.preventDefault();
+  setPickPosition(event.touches[0]);
+}, {passive: false});
+ 
+window.addEventListener('touchmove', (event) => {
+  setPickPosition(event.touches[0]);
+});
+ 
+window.addEventListener('touchend', clearPickPosition);
+
+ 
+
+
+
 
 
 sun.position.z=-270;
@@ -234,7 +346,6 @@ camera.position.z = 0;
 function moveCamera() {
   const t = document.body.getBoundingClientRect().top;
  
-
   mercury.rotation.y-=0.1;
   venus.rotation.y-=0.1;
   earth.rotation.y-=0.1;
@@ -263,6 +374,7 @@ moveCamera();
 function animate(){
   requestAnimationFrame(animate);
 
+  sun.rotation.y += 0.0005;
   mercury.rotation.y += 0.005;
   venus.rotation.y+=0.0045;
   earth.rotation.y+=0.003;
